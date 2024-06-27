366a2458cee70c41adc5298646b0e3a6e6badad8
Konderu Nagateja <konderu.nagateja@globallogic.com>
2024-06-27 11:24:34 +0530

    firstCommit

---
+++ src/main/java/com/example/EmployeeApplication/Controller/RestControllerEx.java
@@ -0,0 +1,15 @@
+package com.example.EmployeeApplication.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("example")
+public class RestControllerEx {
+    @GetMapping("/")
+    public String get()
+    {
+        return "hello world";
+    }
+}
